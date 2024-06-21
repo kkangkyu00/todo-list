ad746dc5eda93ca04fee757d8b338413ca6f92f5
kyuseok <kkangkyu00@gmail.com>
2024-06-21 11:27:38 +0900

    # TabContainer 수정

--- packages/todo-client-react/src/containers/TabContainer/index.tsx
+++ packages/todo-client-react/src/containers/TabContainer/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 import { Home, AddCard, Leaderboard, Settings } from '@mui/icons-material';
 import { classNames } from '@utils';
 
@@ -13,15 +13,17 @@ const pathMap = [
   { icon: <Settings />, path: '/setting' }
 ];
 
-const tabTextVariant = {
+const tabIconVariant = {
   active: { y: -8 },
   inactive: { y: 0 }
 };
 
+const initialVariants = { hiddenState: { display: 'none', transition: { duration: 0 } } };
+
 const TabContainer = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const [variants, setVariants] = useState({});
+  const [variants, setVariants] = useState<Variants>(initialVariants);
 
   useEffect(() => {
     const tick = setTimeout(() => {
@@ -29,15 +31,15 @@ const TabContainer = () => {
       const tabs = document.querySelectorAll('.tab');
       if (!tabs.length || !tabBorder) return;
 
-      const obj: Record<string, unknown> = {};
+      const obj: Variants = {};
       pathMap.forEach(({ path }, index) => {
         const offsetActiveItem = tabs[index].getBoundingClientRect();
         const left = Math.floor(offsetActiveItem.left - (tabBorder.offsetWidth - offsetActiveItem.width) / 2);
         obj[path.slice(1)] = {
-          transform: `translateX(${left - 15}px)`
+          transform: `translateX(${left - 15.2}px)`
         };
       });
-      setVariants(obj);
+      setVariants((prevState) => ({ ...obj, ...prevState }));
     }, 0);
     return () => clearTimeout(tick);
   }, []);
@@ -45,6 +47,11 @@ const TabContainer = () => {
   const handleClickTab = (path: string) => {
     navigate(path);
   };
+
+  const animateKey = useMemo(() => {
+    const isPath = pathMap.filter(({ path }) => location.pathname === path)[0];
+    return isPath ? location.pathname.slice(1) : 'hiddenState';
+  }, [location]);
 
   return (
     <TabWrapper>
@@ -57,11 +64,11 @@ const TabContainer = () => {
               animate={location.pathname === path ? 'active' : 'inactive'}
               onClick={() => handleClickTab(path)}
             >
-              <motion.span variants={tabTextVariant}>{icon}</motion.span>
+              <motion.span variants={tabIconVariant}>{icon}</motion.span>
             </motion.div>
           ))}
         </TabContent>
-        <motion.div className="tab-border" animate={location.pathname.slice(1)} variants={variants}>
+        <motion.div className="tab-border" animate={animateKey} variants={variants}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.625 18.625" height="19.867" width="19.867">
             <path d="M0 0v18.625C.459 6.493 7.17.804 18.625 0z" fillRule="evenodd" />
           </svg>
